0188bc6de52ba99500f4610f309a27c5719dc311
Jürgen Knauth <pubsrc@binary-overflow.de>
2024-04-24 20:42:16 +0200

    Initial commit.

---
+++ src/jk_systools/__init__.py
@@ -0,0 +1,14 @@
+﻿
+
+__version__ = "0.2024.4.24"
+
+
+
+from .IExitCodes import IExitCodes
+from .AppRuntime import AppRuntime
+from .CLIRunCtx import CLIRunCtx
+from .AbstractCLICmd import AbstractCLICmd
+from .AbstractMultiCmdCLIApp import AbstractMultiCmdCLIApp
+
+
+
