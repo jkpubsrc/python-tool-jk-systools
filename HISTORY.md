341bedaa4d18be00efc6cf16b17d7722109bed5f
Jürgen Knauth <pubsrc@binary-overflow.de>
2024-11-03 20:18:39 +0100

    Updated: Dependencies

--- HISTORY.md
+++ HISTORY.md
@@ -2,3 +2,9 @@
 * 2024-04-24
 	* Initial commit.
 
+* 2024-08-14:
+	* Added: Testing
+
+* 2024-11-03:
+	* Updated: Dependencies
+
---
+++ testing/test_cli_df.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+
+import os
+import typing
+import json
+
+from tqdm import tqdm
+
+import jk_typing
+import jk_console
+import jk_utils
+import jk_logging
+import jk_json
+import jk_prettyprintobj
+import jk_dirwalker
+
+from jk_systools.bin.cli_df import main
+
+
+
+main()
+
